f69b39f526a1883aeeaf8d0abee9fcd68d47f939
Emmanuel Ng <emmanuel_ng@outlook.com>
2017-09-22 11:44:06 -0400

    Adds a main method to the hotel manager

--- server/hotels/hotels/impl/HotelManagerImpl.java
+++ server/hotels/hotels/impl/HotelManagerImpl.java
@@ -1,7 +1,49 @@
 package hotels.impl;
 
-import hotels.HotelManager;
+import java.rmi.RMISecurityManager;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
+import hotels.HotelManager;
+import middleware.Middleware;
+import middleware.impl.MiddlewareImpl;
+
+@SuppressWarnings("deprecation")
 public class HotelManagerImpl implements HotelManager {
+
+	public static void main(String[] args) {
+		// Figure out where server is running
+		String server = "localhost";
+		int port = 1099;
+
+		if (args.length == 1) {
+			server = server + ":" + args[0];
+			port = Integer.parseInt(args[0]);
+		} else if (args.length != 0 && args.length != 1) {
+			System.err.println("Wrong usage");
+			System.exit(1);
+		}
+
+		try {
+			// Create a new server object and dynamically generate the stub (client proxy)
+			MiddlewareImpl obj = new MiddlewareImpl();
+			Middleware rm = (Middleware) UnicastRemoteObject.exportObject(obj, 0);
+
+			// Bind the remote object's stub in the registry
+			Registry registry = LocateRegistry.getRegistry(port);
+			registry.rebind("hotels.group20", rm);
+
+			System.out.println("Server ready");
+		} catch (Exception e) {
+			System.err.println("Server exception: " + e.toString());
+			e.printStackTrace();
+		}
+
+		// Create and install a security manager
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new RMISecurityManager());
+		}
+	}
 
 }
