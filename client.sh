7bed03e49c4f14bd51c85a128429f17e5efc1148
Emmanuel Ng <emmanuel.ngchenghin@mail.mcgill.ca>
2017-09-26 13:04:47 -0400

    Adds the client script

---
+++ client.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+function grant_access {
+	if [ $# -eq 1 ]; then
+		local policyFile=$DIR/$1/java.policy
+
+		echo "grant codeBase \"file:"$DIR"/"$1"\" {" > $policyFile
+		echo "	permission java.security.AllPermission;" >> $policyFile
+		echo "};" >> $policyFile
+		echo >> $policyFile
+	fi
+}
+
+export CLASSPATH=$CLASSPATH:$DIR/client/
+export CLASSPATH=$CLASSPATH:$DIR/client/MiddlewareInterface.jar
+
+if [ $# -eq 2 ]; then
+	cd $DIR/client/src/
+	grant_access client/
+	java -Djava.security.policy=java.policy client.Client $1 $2
+else
+	echo "Usage: ./client.sh [servermachine] [port]"
+
+fi
+
