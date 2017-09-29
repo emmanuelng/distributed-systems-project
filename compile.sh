281c2b16fc7b0f0f98730345598ea27efcbac4db
Emmanuel Ng <emmanuel.ngchenghin@mail.mcgill.ca>
2017-09-29 16:52:28 -0400

    Fixes the compile script

--- compile.sh
+++ compile.sh
@@ -5,17 +5,20 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Compile the network classes
 echo "Compiling the network classes..."
 {
-	cd $DIR/network/
-	javac $DIR/network/network/common/*.java
-	javac $DIR/network/network/client/*.java
-	javac $DIR/network/network/server/*.java
-	jar cvf Network.jar -C network/
+	cd $DIR/network/network/
+	javac ./*/*/*.java
+	jar cf Network.jar .
+
+	cp Network.jar $DIR/server/
+	mv Network.jar $DIR/client/
 } #&> /dev/null
 
 # Compile the common classes
-echo "Compiling the common server classes..."
+echo "Compiling the server common classes..."
 {
 	cd $DIR/server/common/
+	export CLASSPATH=$DIR/server/common/
+	export CLASSPATH=$CLASSPATH:$DIR/server/Network.jar
 	javac ./*/*/*.java
 } #&> /dev/null
 
@@ -24,10 +27,10 @@ echo "Compiling the car manager..."
 {
 	cd $DIR/server/cars/
 	export CLASSPATH=$DIR/server/cars/:$DIR/server/common/
-	export CLASSPATH=$CLASSPATH:$DIR/network/Network.jar
+	export CLASSPATH=$CLASSPATH:$DIR/server/Network.jar
 	javac $DIR/server/cars/cars/CarManager.java
 	javac $DIR/server/cars/cars/impl/*.java
-	jar cvf CarManagerInterface.jar cars/*.class
+	jar cf CarManagerInterface.jar cars/*.class
 	mv CarManagerInterface.jar $DIR/server/middleware/
 } #&> /dev/null
 
@@ -36,10 +39,10 @@ echo "Compiling the hotel manager..."
 {
 	cd $DIR/server/hotels/
 	export CLASSPATH=$DIR/server/hotels/:$DIR/server/common/
-	export CLASSPATH=$CLASSPATH:$DIR/network/Network.jar
+	export CLASSPATH=$CLASSPATH:$DIR/server/Network.jar
 	javac $DIR/server/hotels/hotels/HotelManager.java
 	javac $DIR/server/hotels/hotels/impl/*.java
-	jar cvf HotelManagerInterface.jar hotels/*.class
+	jar cf HotelManagerInterface.jar hotels/*.class
 	mv HotelManagerInterface.jar $DIR/server/middleware/
 } #&> /dev/null
 
@@ -48,10 +51,10 @@ echo "Compiling the flight manager..."
 {
 	cd $DIR/server/flights/
 	export CLASSPATH=$DIR/server/flights/:$DIR/server/common/
-	export CLASSPATH=$CLASSPATH:$DIR/network/Network.jar
+	export CLASSPATH=$CLASSPATH:$DIR/server/Network.jar
 	javac $DIR/server/flights/flights/FlightManager.java
 	javac $DIR/server/flights/flights/impl/*.java
-	jar cvf FlightManagerInterface.jar flights/*.class
+	jar cf FlightManagerInterface.jar flights/*.class
 	mv FlightManagerInterface.jar $DIR/server/middleware/
 } #&> /dev/null
 
@@ -60,12 +63,12 @@ echo "Compiling the customer manager..."
 {
 	cd $DIR/server/customers/
 	export CLASSPATH=$DIR/server/customers/:$DIR/server/common/
-	export CLASSPATH=$CLASSPATH:$DIR/network/Network.jar
+	export CLASSPATH=$CLASSPATH:$DIR/server/Network.jar
 	javac $DIR/server/customers/customers/CustomerManager.java
 	javac $DIR/server/customers/customers/impl/*.java
 	# The customers are currently handled in the middleware server.
 	# Uncomment the two following lines if the customers are handled in a separate server.
-	# jar cvf CustomerManagerInterface.jar customers/*.class
+	# jar cvf CustomerManagerInterface.jar customers/*.cexport CLASSPATH=$CLASSPATH:$DIR/network/network/Network.jar
 	# mv CustomerManagerInterface.jar $DIR/server/middleware/
 } #&> /dev/null
 
@@ -75,14 +78,14 @@ echo "Compiling the middleware..."
 	cd $DIR/server/middleware/
 	export CLASSPATH=$CLASSPATH:$DIR/server/middleware/
 	export CLASSPATH=$CLASSPATH:$DIR/server/common/
-	export CLASSPATH=$CLASSPATH:$DIR/network/Network.jar
+	export CLASSPATH=$CLASSPATH:$DIR/server/Network.jar
 	export CLASSPATH=$CLASSPATH:$DIR/server/customers
 	export CLASSPATH=$CLASSPATH:$DIR/server/middleware/CarManagerInterface.jar
 	export CLASSPATH=$CLASSPATH:$DIR/server/middleware/FlightManagerInterface.jar
 	export CLASSPATH=$CLASSPATH:$DIR/server/middleware/HotelManagerInterface.jar
 	javac $DIR/server/middleware/middleware/Middleware.java
 	javac $DIR/server/middleware/middleware/impl/*.java
-	jar cvf MiddlewareInterface.jar middleware/*.class
+	jar cf MiddlewareInterface.jar middleware/*.class
 	mv MiddlewareInterface.jar $DIR/client/
 } #&> /dev/null
 
@@ -90,6 +93,7 @@ echo "Compiling the middleware..."
 echo "Compiling the client..."
 {
 	export CLASSPATH=$DIR/client/MiddlewareInterface.jar
+	export CLASSPATH=$CLASSPATH:$DIR/client/Network.jar
 	javac $DIR/client/src/client/*.java
 } #&> /dev/null
 
